8e4c98368dff662ee3c9a665ba89a9073aa24481
yw22 <youngwook.dev@gmail.com>
2022-03-08 20:11:38 +0900

    update

---
+++ Baekjoon/10926.py
@@ -0,0 +1,5 @@
+# 10926
+import sys
+a = sys.stdin.readline().strip()
+
+print(a + "??!")
--- Baekjoon/파이썬정리.py
+++ Baekjoon/파이썬정리.py
@@ -383,3 +383,15 @@ counter = Counter(['red', 'blue', 'red', 'green', 'blue', 'blue'])
 print(counter['blue'])
 print(counter['green'])
 print(dict(counter))
+
+import math
+
+# 최소 공배수(LCM)를 구하는 함수
+def lcm(a, b):
+  return a * b // math.gcd(a, b)
+
+a = 21
+b = 14
+
+print(math.gcd(a, b)) # 최대 공약수(GCD) 계산
+print(lcm(a, b)) #최소 공배수(LCM) 계산

a1db0507165a9b496b412d5b06323dfdf66da729
yw22 <youngwook.dev@gmail.com>
2022-03-21 21:51:36 +0900

    update

---
+++ Baekjoon/별찍기_10.py
@@ -0,0 +1,42 @@
+# 2447번
+
+# 재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
+# 크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
+# ***
+# * *
+# ***
+# N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 
+
+
+
+def draw_star(n):
+  global Map
+
+  #  3 * 3 정사각형의 별 모향 만들기
+  if n == 3:
+    Map[0][:3] = Map[2][:3] = [1] * 3
+    Map[1][:3] = [1, 0, 1]
+    return
+
+  a = n // 3
+  draw_star(n // 3)
+  for i in range(3):
+    for j in range(3):
+      if i == 1 and j == 1:
+        continue
+      for k in range(a):
+        Map[a * i + k][a * j : a * (j + 1)] = Map[k][:a]
+
+n = int(input())
+
+Map = [[0 for i in range(n)] for i in range(n)]
+
+draw_star(n)
+
+for i in Map:
+  for j in i:
+    if j:
+      print('* ', end = '')
+    else:
+      print(' ', end = ' ')
+  print()

608c87d3a1a2cb6143a80732e3525d4f8c7a8ac8
yw22 <youngwook.dev@gmail.com>
2022-03-29 22:45:19 +0900

    # 직각삼각형 [피타고라스의 정리에 대해 배우는 문제]

---
+++ CodingTest/Baekjoon/단계별문제/기본수학2/4154.py
@@ -0,0 +1,9 @@
+# 직각삼각형 [피타고라스의 정리에 대해 배우는 문제]
+
+while True:
+  a = list(map(int, input().split()))
+  a.sort()
+  print(a)
+  if a[0] == a[1] == a[2] == 0:
+    break
+  print('right') if a[0] ** 2 + a[1] ** 2 == a[2] ** 2 else print('wrong')

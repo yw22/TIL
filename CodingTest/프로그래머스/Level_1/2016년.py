f4e7c4a1aefb0ace0ed31eeb8b7c203b0f04cecf
yw22 <youngwook.dev@gmail.com>
2022-04-07 21:06:56 +0900

    check : 0

---
+++ CodingTest/프로그래머스/Level_1/2016년.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    days = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    day = 0
+    for i in range(1, a):
+        if i == 1 or i == 3 or i == 5 or i == 7 or i == 8 or i == 10 or i == 12:
+            day += 31
+        elif i == 2:
+            day += 29
+        else:
+            day += 30
+    day += b
+        
+    return days[day % 7]

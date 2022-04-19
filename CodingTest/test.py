5ca703213b1243349d53e58fba3bab2d4ca4c065
yw22 <youngwook.dev@gmail.com>
2022-04-19 23:32:34 +0900

    update

--- CodingTest/test.py
+++ CodingTest/test.py
@@ -1,12 +1,10 @@
-def solution(arr1, arr2):
-    answer = [[0] * len(arr1[0]) for _ in range(len(arr1))]
-    for i in range(len(arr1)):
-        for j in range(len(arr2[0])):
-            for k in range(len(arr1[0])):
-                answer[i][j] += (arr1[i][k] * arr2[k][j])
-    return answer
+def is_odd(n):
+    if n % 2 == 0:
+        print(str(n) + "은 짝수입니다")
+        return True
+    else:
+        print(str(n) + "은 짝수가 아닙니다.")
+        return False
 
-
-arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]    
-print(solution(arr1,arr2))
+is_odd(10)
+is_odd(17)

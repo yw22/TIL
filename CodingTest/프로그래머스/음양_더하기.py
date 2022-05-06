3247947aa40cf5268bfbee841500d2c4988a344e
yw22 <youngwook.dev@gmail.com>
2022-05-07 05:58:12 +0900

    remove

--- CodingTest/프로그래머스/음양_더하기.py
+++
@@ -1,9 +0,0 @@
-def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(absolutes)):
-        if signs[i]:
-            answer += int(absolutes[i])
-        else:
-            answer += (int(absolutes[i]) * -1)
-        
-    return answer

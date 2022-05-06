e1eb63c109259bdfea850b5bb06c3ad29cb9117c
yw22 <youngwook.dev@gmail.com>
2022-05-06 18:31:21 +0900

    check : 1

--- CodingTest/프로그래머스/Level_1/크레인_인형뽑기_게임.swift
+++ CodingTest/프로그래머스/Level_1/크레인_인형뽑기_게임.swift
@@ -1,24 +1,23 @@
+import Foundation
 
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+  var arr = board
+  var characters: [Int] = []
+  var count = 0
+  moves.forEach {
+    for i in 0 ..< board.count {
+      if arr[i][$0 - 1] != 0 {
+        if characters.last == arr[i][$0 - 1]{
+          characters.removeLast()
+          count += 2
+        } else { characters += [arr[i][$0 - 1]] }
+        arr[i][$0 - 1] = 0
+        break
+      }
+    }
+  }
+  return count
+}
 let board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 let moves = [1,5,3,5,1,2,1,4]
-
-var a = board
-var num : [Int] = []
-for i in moves{
-    for j in 0...4{
-        if a[j][i - 1] != 0 {
-            num.append(a[j][i - 1])
-            a[j][i - 1] = 0
-        }
-    }
-}
-print(a)
-print(num)
-for i in 1...((num.count) - 1){
-    if num[i - 1] == num[i]{
-        num.remove(at : i - 1)
-        num.remove(at : i - 1)
-    }
-}
-
-print(num.count)
+print(solution(board, moves))

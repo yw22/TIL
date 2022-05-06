85aae082469cdba3a8f70f9b97b9eca36a6634f8
yw22 <youngwook.dev@gmail.com>
2022-05-07 05:55:41 +0900

    check : 0

---
+++ CodingTest/프로그래머스/Level_1/직사각형_별찍기.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+// 한줄에 n과 m을 입력받기
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+// n x m의 (*)찍기
+let (n,m) = (input[1], input[0])
+for _ in 0 ..< n{
+  for _ in 0 ..< m{
+    print("*", terminator: "")
+  }
+  print()
+}
+
+
+// Array(repeating: ,count : ) -> 배열 초기화하여 joined으로 병합한후 m줄 반복
+
+for _ in 0..<input[0] {
+  print(Array(repeating: "*", count: 10).joined())
+}

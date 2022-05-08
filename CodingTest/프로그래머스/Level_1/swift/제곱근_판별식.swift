c460a471f7194321d71cb0974c048b085b870b09
yw22 <youngwook.dev@gmail.com>
2022-05-09 08:51:39 +0900

    check : 1

--- CodingTest/프로그래머스/Level_1/swift/제곱근_판별식.swift
+++ CodingTest/프로그래머스/Level_1/swift/제곱근_판별식.swift
@@ -6,3 +6,9 @@ func solution(_ n:Int64) -> Int64 {
     }
     return -1
 }
+
+
+// func solution(_ n:Int64) -> Int64 {
+//     let t = Int64(sqrt(Double(n)))
+//     return t * t == n ? (t+1)*(t+1) : -1
+// }

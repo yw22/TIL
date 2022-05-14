3a194f72d1fc4bb8ab5381f6889963563702f4d4
yw22 <youngwook.dev@gmail.com>
2022-05-14 23:32:36 +0900

    check : 0

---
+++ CodingTest/프로그래머스/Level_2/Swift/튜플.swift
@@ -0,0 +1,18 @@
+func solution(_ s:String) -> [Int] {
+  var s = s
+  var answer = [Int]()
+  s.removeFirst(2)
+  s.removeLast(2)
+
+  s.components(separatedBy: "},{")
+    .map { $0.split(separator: ",").map { Int(String($0))! } }
+    .sorted { $0.count < $1.count }
+    .forEach {
+      $0.forEach {
+        if !answer.contains($0) {
+          answer.append($0)
+        }
+      }
+    }
+  return answer
+}

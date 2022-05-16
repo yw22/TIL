80e409f4a3804b0edd535d0a23ed48c9d5c5326b
yw22 <youngwook.dev@gmail.com>
2022-05-16 21:43:00 +0900

    check : 0

---
+++ CodingTest/프로그래머스/Level_2/Swift/H_Index.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ citations:[Int]) -> Int {
+    let citations = citations.sorted(by: >)
+    var result = 0
+
+    for i in 0..<citations.count {
+        if i + 1 <= citations[i] {
+            result = i + 1
+        } else {
+            break
+        }
+    }
+
+    return result
+}

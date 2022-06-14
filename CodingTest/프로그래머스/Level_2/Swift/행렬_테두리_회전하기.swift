b93136ace639152d12d6f1c313e5db87b646fcf1
yw22 <youngwook.dev@gmail.com>
2022-06-14 22:57:24 +0900

    check : 1

--- CodingTest/프로그래머스/Level_2/Swift/행렬_테두리_회전하기.swift
+++ CodingTest/프로그래머스/Level_2/Swift/행렬_테두리_회전하기.swift
@@ -1,46 +1,72 @@
+import Foundation
+
 func solution(_ rows:Int, _ columns:Int, _ queries:[[Int]]) -> [Int] {
-
-    var matrix = [[Int]]()
-    var ans = [Int]()
-    for r in 0..<rows {
-        matrix.append(Array(r*columns + 1...r*columns + columns))
+    var arr = Array(repeating: Array(repeating: 0, count: columns), count: rows )
+    var count = 1
+    for i in 0..<rows {
+        for j in 0..<columns {
+            arr[i][j] = count
+            count += 1
+        }
     }
-
-    for q in queries {
-        let miny = q[0] - 1
-        let minx = q[1] - 1
-        let maxy = q[2] - 1
-        let maxx = q[3] - 1
-        //빼버릴 값 (좌상단)
-        let pivott = matrix[miny][minx]
-        //회전의 최소값 추적용
-        var minn = pivott
-
-	//좌상단에서 하단으로 가면서 퍼즐을 한칸씩 위로 올림
-        for y in miny..<maxy {
-            minn = min(matrix[y+1][minx], minn)
-            matrix[y][minx] = matrix[y+1][minx]
+    var minArr = [Int]()
+    
+    
+    for i in queries {
+        var min = Int.max
+        var x = i[0] - 1
+        var y = i[1] - 1
+        let minRow = i[0] - 1
+        let minCol = i[1] - 1
+        let maxRow = i[2] - 1
+        let maxCol = i[3] - 1
+        
+        
+        var temp1 = arr[x][y]
+        var temp2 = arr[x][y]
+        // 위 ->
+        while true {
+            guard y < maxCol else { break }
+            min = min < temp1 ? min : temp1
+            temp1 = temp2
+            temp2 = arr[x][y + 1]
+            arr[x][y + 1] = temp1
+            y += 1
         }
-        //좌하단에서부터 오른쪽으로 가며 퍼즐을 한칸씩 왼쪽으로
-        for x in minx..<maxx {
-            minn = min(matrix[maxy][x+1], minn)
-            matrix[maxy][x] = matrix[maxy][x+1]
+        
+        // 오른쪽 아래로
+        
+        while true {
+            guard x < maxRow else { break }
+            min = min < temp1 ? min : temp1
+            temp1 = temp2
+            temp2 = arr[x + 1][y]
+            arr[x + 1][y] = temp1
+            x += 1
         }
-        //우하단에서 위로 가며 퍼즐을 한칸씩 아래로
-        for y in (miny+1...maxy).reversed() {
-            minn = min(minn, matrix[y-1][maxx])
-            matrix[y][maxx] = matrix[y-1][maxx]
+        
+        // 아래 <-
+        while true {
+            guard y > minCol else { break }
+            min = min < temp1 ? min : temp1
+            temp1 = temp2
+            temp2 = arr[x][y - 1]
+            arr[x][y - 1] = temp1
+            y -= 1
         }
-        //우상단에서 왼쪽으로 가며 퍼즐을 한칸씩 오른쪽으로
-        for x in (minx+1...maxx).reversed() {
-            minn = min(minn, matrix[miny][x-1])
-            matrix[miny][x] = matrix[miny][x-1]
+        
+        // 왼쪽 위로
+        while true {
+            guard x > minRow else { break }
+            min = min < temp1 ? min : temp1
+            temp1 = temp2
+            temp2 = arr[x - 1][y]
+            arr[x - 1][y] = temp1
+            x -= 1
         }
-        //마지막엔 빼둔값을 한칸 오른쪽에 넣어주면 완성
-        matrix[miny][minx+1] = pivott
-        //회전한 숫자중 최솟값을 배열에 더해준다
-        ans.append(minn)
+        
+        minArr.append(min)
     }
-
-    return ans
+    
+    return minArr
 }

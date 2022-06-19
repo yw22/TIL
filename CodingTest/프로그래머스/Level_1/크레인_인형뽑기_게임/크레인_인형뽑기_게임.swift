f6df99fff4a6744dbaa22ac858251930845d22bb
yw22 <youngwook.dev@gmail.com>
2022-06-19 21:03:33 +0900

    Complete

--- CodingTest/프로그래머스/Level_1/크레인_인형뽑기_게임/크레인_인형뽑기_게임.swift
+++ CodingTest/프로그래머스/Level_1/크레인_인형뽑기_게임/크레인_인형뽑기_게임.swift
@@ -2,15 +2,17 @@ import Foundation
 
 func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
   var arr = board
-  var characters: [Int] = []
+  var stackArr: [Int] = []
   var count = 0
   moves.forEach {
     for i in 0 ..< board.count {
       if arr[i][$0 - 1] != 0 {
-        if characters.last == arr[i][$0 - 1]{
-          characters.removeLast()
+        if stackArr.last == arr[i][$0 - 1]{
+          stackArr.removeLast()
           count += 2
-        } else { characters += [arr[i][$0 - 1]] }
+        } else { 
+          stackArr += [arr[i][$0 - 1]] 
+        }
         arr[i][$0 - 1] = 0
         break
       }
@@ -18,6 +20,3 @@ func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
   }
   return count
 }
-let board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
-let moves = [1,5,3,5,1,2,1,4]
-print(solution(board, moves))

2f116fc7ad651daada5a0899ac254fa4a9559633
yw22 <youngwook.dev@gmail.com>
2022-06-20 23:52:11 +0900

    Complete

--- CodingTest/프로그래머스/Level_1/숫자_문자열과_영단어/숫자_문자열과_영단어.swift
+++ CodingTest/프로그래머스/Level_1/숫자_문자열과_영단어/숫자_문자열과_영단어.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+//-- 사전(dictionary)
 func solution(_ s : String) -> Int {
   let numDic = ["zero" : "0", "one" : "1","two" : "2","three" : "3","four" : "4","five" : "5","six" : "6","seven" : "7" ,"eight" : "8","nine" : "9"]
   var answer = s
@@ -7,5 +8,14 @@ func solution(_ s : String) -> Int {
     answer = answer.replacingOccurrences(of: value.key, with: value.value)
   }
   return Int(answer)!
-  
+}
+
+//-- 배열과 인덱스
+func solution1(_ s:String) -> Int {
+    let arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    var s = s
+    for i in 0..<arr.count{
+        s = s.replacingOccurrences(of: arr[i], with: "\(i)")
+    }
+    return Int(s)!
 }

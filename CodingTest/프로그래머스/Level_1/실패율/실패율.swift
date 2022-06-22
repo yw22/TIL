418b79f15252e34cdeedec11923bbc5d42407e1c
yw22 <youngwook.dev@gmail.com>
2022-06-22 23:56:01 +0900

    check: 1

--- CodingTest/프로그래머스/Level_1/실패율/실패율.swift
+++ CodingTest/프로그래머스/Level_1/실패율/실패율.swift
@@ -41,3 +41,122 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
   }
   return arr
 }
+
+
+//------------------------------------------------------------------------------------------------------
+
+func solution(_ n:Int, _ stages:[Int]) -> [Int] {
+    var total = Float(stages.count)
+    var falseArr = Array<Float>(repeating: 0, count: n)
+    var arr = [Int]()
+    for i in stages {
+        if i <= n { arr.append(i) }
+    }
+    for i in arr {
+        falseArr[i - 1] += 1
+    }
+    
+//    var falseArr = Array<Float>(repeating: 0, count: n)
+//        stages.filter { $0 <= n }.forEach { falseArr[$0 - 1] += 1 }
+//
+    
+    
+//    var falseArr = Array<Float>(repeating: 0, count: n)
+//    for i in 1...n {
+//        if stages.contains(i){
+//            falseArr[i - 1] = Float(stages.filter{$0 == i}.count)
+//        } else { falseArr.append(0)}
+//    }
+    
+    
+    for i in 1...n {
+        let tmp = falseArr[i - 1]
+        falseArr[i - 1] = falseArr[i - 1] / total
+        total -= tmp
+    }
+    
+
+    return falseArr.enumerated().sorted{ $0.element > $1.element}.map{$0.offset + 1}
+}
+
+//----------------------------------------------------------------------------------------------------
+
+func solution(_ n:Int, _ stages:[Int]) -> [Int] {
+    // 각 단계별로 실패율 계산
+    var total = Float(stages.count)
+    var falseArr = Array<Float>(repeating: 0, count: n)
+    stages.filter { $0 <= n }.forEach { falseArr[$0 - 1] += 1 }
+    
+    for i in 1...n {
+        if total != 0{
+            let temp = falseArr[i - 1]
+            falseArr[i - 1] /= total
+            total -= temp
+        }
+    }
+    
+    // 실패율이 높은 순으로 정렬, 실패율이 같을경우 스테이지가 낮은 순으로 정렬
+    
+    var dic = [Int: Float]()
+    
+    for i in 1...n {
+        dic[i] = falseArr[i - 1]
+    }
+
+    
+    return dic.sorted(by: <).sorted{$0.1 > $1.1}.map{$0.key}
+}
+
+
+
+//------------ 내 코드의 문제점
+
+func falseCode () {
+    var falseArr = Array<Float>(repeating: 0, count: n)
+    stages.filter { $0 <= n }.forEach { falseArr[$0 - 1] += 1 }
+    // for i in stages {
+    //     if i <= n { arr.append(i) }
+    // }
+    // for i in arr {
+    //     falseArr[i - 1] += 1
+    // }
+}
+
+func betterCode() {
+    var falseArr = Array<Float>(repeating: 0, count: n)
+    for i in 1...n {
+        falseArr[i - 1] = Float(stages.filter{$0 == i}.count)
+    }
+}
+
+
+//-------------(51.76ms, 25.6MB)
+
+import Foundation
+
+func solution(_ n:Int, _ stages:[Int]) -> [Int] {
+    // 각 단계별로 실패율 계산
+    var total = Float(stages.count)
+    var dicFalse = [Int: Float]()
+    for i in stages {
+        dicFalse[i] = (dicFalse[i] ?? 0) + 1
+    }
+    
+    for i in 1...n {
+        if total != 0{
+            let temp = dicFalse[i] ?? 0
+            dicFalse[i] = (dicFalse[i] ?? 0) / total
+            total -= temp
+        }
+    }
+    
+    dicFalse[n + 1] = nil
+    
+    // 실패율이 높은 순으로 정렬, 실패율이 같을경우 스테이지가 낮은 순으로 정렬
+
+    
+    return dicFalse.sorted(by: <).sorted{$0.1 > $1.1}.map{$0.key}
+}
+
+
+solution(5, [2, 1, 2, 6, 2, 4, 3, 3])

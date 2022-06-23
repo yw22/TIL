86c567e1d85ff9a4a3e52ff7758f3d685c609d90
yw22 <youngwook.dev@gmail.com>
2022-06-23 15:06:44 +0900

    Complete

--- CodingTest/프로그래머스/Level_1/소수찾기/소수찾기.swift
+++ CodingTest/프로그래머스/Level_1/소수찾기/소수찾기.swift
@@ -1,6 +1,6 @@
 //-- 에라토스테네스의 체
 func solution(_ n:Int) -> Int {
-  guard n != 2 else{ return 1 }
+  guard n > 2 else{ return 1 }
   var arr = Array(repeating: 1, count: n + 1)
   for i in 2...(n / 2) {
     var count = 2
@@ -10,4 +10,19 @@ func solution(_ n:Int) -> Int {
     }
   }
   return arr[2...n].filter{$0 == 1}.reduce(0, +)
+}
+
+//-- 반복문
+
+func solution(_ n:Int) -> Int {
+  var count = 1
+  for i in 3...n{
+      for j in 2..<i {
+          if i % j == 0 {
+            count += 1
+            break
+          }
+      }
+  }
+  return count
 }

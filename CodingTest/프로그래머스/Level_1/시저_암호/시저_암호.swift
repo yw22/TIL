5e1495a9540d7379e72050ec3523246104309dd4
yw22 <youngwook.dev@gmail.com>
2022-06-23 15:07:28 +0900

    check: 1

--- CodingTest/프로그래머스/Level_1/시저_암호/시저_암호.swift
+++ CodingTest/프로그래머스/Level_1/시저_암호/시저_암호.swift
@@ -1,62 +1,65 @@
-struct password1 {
-  func lowerAscString(_ a : UInt8) -> String {
-    var num = a
-    if a > 90 { num -= 26 }
-    return String(UnicodeScalar(num))
-  }
+import Foundation
 
-  func upperAscString(_ a : UInt8) -> String {
-    var num = a
-    if a > 122 { num -= 26 }
-    return String(UnicodeScalar(num))
-  }
-
-  func solution(_ s:String, _ n:Int) -> String {
-    var newString = ""
-    s.forEach {
-      var num = $0.asciiValue!
-      if (num > 64 && num < 91) {
-        num += UInt8(n)
-        newString += lowerAscString(num)
-      } else if (num > 96 && num < 123) {
-        num += UInt8(n)
-        newString += upperAscString(num)
-      } else {
-        newString += " "
-      }
+//-- 유니코드 활용
+func solution(_ s:String, _ n:Int) -> String {
+    var result = ""
+    let s = Array(s).map{String($0)}
+    s.forEach{ char in
+        switch char{
+        case("A"..."Z"):
+            let num = Int(UnicodeScalar(char)!.value) - 65 + n
+            result += String(UnicodeScalar((num % 26) + 65)!)
+        case("a"..."z"):
+            let num = Int(UnicodeScalar(char)!.value) - 97 + n
+            result += String(UnicodeScalar((num % 26) + 97)!)
+        default:
+            result += char
+        }
+    
     }
     
-    return newString
-  }
+    return result
 }
 
-//-- switch 구문과 utf8을 활용한 방법
-struct password2 {
-  func solution(_ s:String, _ n:Int) -> String {
-    return s.utf8.map {
-        var code = Int($0)
-        switch code {
-            case 65...90:
-                code = (code + n - 65) % 26 + 65
-            case 97...122:
-                code = (code + n - 97) % 26 + 97
-            default:
-                break
+
+//-- utf8 활용
+import Foundation
+
+func solution(_ s:String, _ n:Int) -> String {
+    var result = ""
+    let s = s.utf8.map{Int($0)}
+    s.forEach{ 
+        switch $0{
+        case 65...91:
+            let code = ($0 + n - 65) % 26 + 65
+            result += String(UnicodeScalar(code)!)
+        case 97...123:
+            let code = ($0 + n - 97) % 26 + 97
+            result += String(UnicodeScalar(code)!)
+            
+        default:
+            result += String(UnicodeScalar($0)!)
         }
-        return String(UnicodeScalar(code)!)
-    }.joined()
-  }
-}
-
-//-- index를 활용한 방법
-struct password3 {
-  func solution(_ s:String, _ n:Int) -> String {
-    let alphabets = "abcdefghijklmnopqrstuvwxyz".map { $0 }
-    return String(s.map {
-        guard let index = alphabets.firstIndex(of: Character($0.lowercased())) else { return $0 }
-        let letter = alphabets[(index + n) % 26]
-        return $0.isUppercase ? Character(letter.uppercased()) : letter
-        })
+    
     }
+    
+    return result
 }
 
+//-- utf8 활용2
+
+func solution(_ s:String, _ n:Int) -> String {
+    let s = s.utf8.map{
+          switch Int($0){
+          case 65...91:
+              let code = ($0 + n - 65) % 26 + 65
+          case 97...123:
+              let code = ($0 + n - 97) % 26 + 97
+          default:
+              return " "
+          }
+          return String(UnicodeScalar(code)!)
+      }
+    
+    return s.joined(" ")
+}

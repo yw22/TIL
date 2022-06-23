d005e89106d8bc30facbd1de4b80ab467cc12a98
yw22 <youngwook.dev@gmail.com>
2022-06-23 15:11:57 +0900

    Complete

--- CodingTest/프로그래머스/Level_1/수박수박수/수박수박수.swift
+++ CodingTest/프로그래머스/Level_1/수박수박수/수박수박수.swift
@@ -1,4 +1,10 @@
 func solution(_ n:Int) -> String {
+    var str = String(repeating: "수박", count: n/2)
+    str.append(n % 2 == 0 ? "" : "수")
+    return str
+}
+
+func solution(_ n:Int) -> String {
     let (str1,str2) = ("수", "박")
     var answer = ""
     for i in 1...n {
@@ -8,11 +14,6 @@ func solution(_ n:Int) -> String {
     return answer
 }
 
-func solution(_ n:Int) -> String {
-    var str1 = String(repeating: "수박", count: n/2)
-    var str2 = n % 2 == 0 ? "" : "수"
-    return str1 + str2
-}
 
 func solution(_ n:Int) -> String {
   var result = (0 ..< n).map{($0 % 2 == 0 ? "수" : "박")}.joined()
